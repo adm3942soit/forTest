be4880dd03dfa60acb25d2275f877a3639bd32ec
Oksana Dudnik <adm3942soit@gmail.com>
2016-07-27 15:06:14 +0300

    implemented test for RestTester

---
+++ src/main/java/com/test/rest/RestTester.java
@@ -0,0 +1,255 @@
+package com.test.rest;
+
+import com.credentials.Credentials;
+import com.test.json.JacksonFeature;
+import org.junit.BeforeClass;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.io.File;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by oksdud on 27.07.2016.
+ */
+public class RestTester {
+    public static Boolean GLASSFISH_ON = false;
+    public static Client client;
+    public static String current;
+    public static String resources;
+    public static String applicationName;
+    public static String nameConfig = "ecom-dev.properties";
+    public static void getCurrentDirs(){
+        try {
+            current = new File(".").getCanonicalPath();
+            current = current.replaceAll("\\\\", "/");
+            resources = current + "/src/main/resources";
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    public static Context ctx;
+
+    private static EJBContainer container;
+    private static Map<String, NewCookie> cookieMap;
+
+
+    public static Context getContext() {
+        return ctx;
+    }
+
+    public static void prepareData() {
+        Map<String, Object> properties = new HashMap<String, Object>();
+
+        String pathToGlassfishRoot = System.getenv("GLASSFISH_HOME");
+
+        if (pathToGlassfishRoot == null || pathToGlassfishRoot.isEmpty()) {
+            System.out.println("You have input environment variable GLASSFISH_HOME");
+            GLASSFISH_ON = false;
+        } else GLASSFISH_ON = true;
+        if (!GLASSFISH_ON) return;
+
+        client = ClientBuilder.newClient().register(JacksonFeature.class);
+
+
+        pathToGlassfishRoot = pathToGlassfishRoot + File.separator + "glassfish" + File.separator;
+        properties.put(
+                "org.glassfish.ejb.embedded.glassfish.installation.root",
+                pathToGlassfishRoot
+        );
+        String pathToDomain = pathToGlassfishRoot + "domains" + File.separator + "backend-dom1";
+        properties.put(
+                "org.glassfish.ejb.embedded.glassfish.instance.root",
+                pathToDomain
+        );
+        properties.put(
+                "org.glassfish.ejb.embedded.glassfish.configuration.file",
+                pathToDomain + File.separator + "config" + File.separator + "domain.xml"
+        );
+
+        properties.put(EJBContainer.APP_NAME, applicationName);
+
+        String fileName = "";
+
+        fileName = System.getenv("lpb.ecom.config");
+
+        if (fileName == null || fileName.isEmpty()) {
+            System.out.println("You have input environment variable lpb.ecom.config with value path to file ecom-dev.properties");
+            try {
+                fileName = pathToDomain + File.separator + "config" + File.separator + nameConfig;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
+        System.setProperty("user.script", fileName);
+
+        container = EJBContainer.createEJBContainer(properties);//
+        ctx = container.getContext();
+/*
+        try {
+
+            transactionalCaller = (Caller)
+                    ctx.lookup("java:global/ecom-rest/test/TransactionBean");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+*/
+
+    }
+    private static URI getBaseURI(String url) {
+        return UriBuilder.fromUri(url).build();
+    }
+    public static void setSessionUserNamePassword(String urlForLogin) {
+        if (!GLASSFISH_ON) return;
+        try {
+            autorization(urlForLogin,Credentials.getUserName(Credentials.MERCHANT), Credentials.MERCHANT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    public static void setSessionUserNamePassword(String urlForLogin, String userName, String password) {
+        if (!GLASSFISH_ON) return;
+        try {
+            autorization(urlForLogin,userName, password);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private static void autorization(String urlForLogin,String userName, String key) {
+        try {
+            WebTarget target = RestTester.getClient().target(getBaseURI(urlForLogin));
+            Response clientResponse = target
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.json("{\"name\":\"" + userName + "\" ,\"password\":\"" + Credentials.getUserPassword(key) + "\"}"), Response.class);
+            cookieMap = clientResponse.getCookies();
+            System.out.println("Received cookie: " + cookieMap.get("JSESSIONID").getValue());
+
+        } catch (Exception e) {
+            System.out.println("\n\tGot exception: " + e.getMessage());
+            System.out.println(e.getCause().toString());
+            if (e.getMessage() != null && e.getMessage().contains("Connection refused")) {
+                System.out.println("Glassfish server not started");
+                RestTester.GLASSFISH_ON = false;
+            }
+
+        }
+
+    }
+
+    public static void checkResource(
+            String urlForResource,
+            String nameRequest,
+            String pathInResource,
+            String parameterToResource,
+            Entity entity
+    ) {
+        if (!GLASSFISH_ON) return;
+        try {
+           // setArguments(urlForResource,urlForLogin,"get",null,"id","21213832");
+            WebTarget target = RestTester.getClient().target(getBaseURI(urlForResource));
+            if(pathInResource!=null){
+                if(parameterToResource==null)
+                    target=target.path(pathInResource);
+                else
+                    target=target.path("{"+pathInResource+"}").resolveTemplate(pathInResource,parameterToResource);
+            }
+            System.out.println("Received cookie: " + cookieMap);
+            Invocation.Builder invocationBuilder = target
+                    .request(MediaType.APPLICATION_JSON)
+                    .cookie("JSESSIONID", cookieMap.get("JSESSIONID").getValue());
+            Response clientResponse = null;
+            switch (nameRequest) {
+                case "GET":
+                case "get":
+                    clientResponse = invocationBuilder.get();
+                    break;
+                case "PUT":
+                case "put":
+                    clientResponse = invocationBuilder.put(entity);
+                    break;
+                case "POST":
+                case "post":
+                    clientResponse = invocationBuilder.post(Entity.entity(entity, MediaType.APPLICATION_JSON), Response.class);
+                    break;
+                default:
+                    nameRequest = "get";
+                    clientResponse = invocationBuilder.get();
+                    break;
+            }
+            String jsonResponse = clientResponse.readEntity(String.class);
+            System.out.println("Received response from persister: " + jsonResponse);
+        } catch (Exception e) {
+            System.out.println("\n\tGot exception: " + e.getMessage());
+            System.out.println(e.getCause());
+            if (e.getMessage() != null && e.getMessage().contains("Connection refused")) {
+                System.out.println("Glassfish server not started");
+                RestTester.GLASSFISH_ON = false;
+            }
+
+        }
+
+        return ;
+    }
+
+    public static Boolean getGlassfishOn() {
+        return GLASSFISH_ON;
+    }
+
+    public static Client getClient() {
+        return client;
+    }
+
+    public static String getCurrent() {
+        return current;
+    }
+
+    public static String getResources() {
+        return resources;
+    }
+
+    public static String getApplicationName() {
+        return applicationName;
+    }
+
+    public static String getNameConfig() {
+        return nameConfig;
+    }
+
+    public static void setGlassfishOn(Boolean glassfishOn) {
+        GLASSFISH_ON = glassfishOn;
+    }
+
+    public static void setClient(Client client) {
+        RestTester.client = client;
+    }
+
+    public static void setCurrent(String current) {
+        RestTester.current = current;
+    }
+
+    public static void setResources(String resources) {
+        RestTester.resources = resources;
+    }
+
+    public static void setApplicationName(String applicationName) {
+        RestTester.applicationName = applicationName;
+    }
+
+    public static void setNameConfig(String nameConfig) {
+        RestTester.nameConfig = nameConfig;
+    }
+}
---
+++ src/test/java/com/test/rest/RestResourcesTester.java
@@ -0,0 +1,372 @@
+package com.test.rest;
+
+
+import com.credentials.Credentials;
+import com.test.orderTests.Order;
+import com.test.orderTests.OrderRunner;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.*;
+import org.junit.runner.RunWith;
+
+import javax.naming.Context;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
+/**
+ * Created by oksdud on 28.06.2016.
+ */
+@RunWith(OrderRunner.class)
+@Slf4j
+
+public class RestResourcesTester {
+    private static String nameXmlFile;
+    private static String urlForResource;
+    private static String urlForLogin;
+    private static String nameRequest;
+    private static String pathInResource;
+    private static String parameterToResource;
+
+    public static String getNameXmlFile() {
+        return nameXmlFile;
+    }
+
+    public static void setNameXmlFile(String nameXmlFile) {
+        RestResourcesTester.nameXmlFile = nameXmlFile;
+    }
+
+    public static String getUrlForResource() {
+        return urlForResource;
+    }
+
+    public static void setUrlForResource(String urlForResource) {
+        RestResourcesTester.urlForResource = urlForResource;
+    }
+
+    public static String getUrlForLogin() {
+        return urlForLogin;
+    }
+
+    public static void setUrlForLogin(String urlForLogin) {
+        RestResourcesTester.urlForLogin = urlForLogin;
+    }
+
+    public static String getNameRequest() {
+        return nameRequest;
+    }
+
+    public static void setNameRequest(String nameRequest) {
+        RestResourcesTester.nameRequest = nameRequest;
+    }
+
+    public static String getPathInResource() {
+        return pathInResource;
+    }
+
+    public static void setPathInResource(String pathInResource) {
+        RestResourcesTester.pathInResource = pathInResource;
+    }
+
+    public static String getParameterToResource() {
+        return parameterToResource;
+    }
+
+    public static void setParameterToResource(String parameterToResource) {
+        RestResourcesTester.parameterToResource = parameterToResource;
+    }
+
+    public static Entity<?> getEntity() {
+        return entity;
+    }
+
+    public static void setEntity(Entity<?> entity) {
+        RestResourcesTester.entity = entity;
+    }
+
+    public static String getUrlPaymentResource() {
+        return urlPaymentResource;
+    }
+
+    public static void setUrlPaymentResource(String urlPaymentResource) {
+        RestResourcesTester.urlPaymentResource = urlPaymentResource;
+    }
+
+    public static String getUrlSessionResource() {
+        return urlSessionResource;
+    }
+
+    public static void setUrlSessionResource(String urlSessionResource) {
+        RestResourcesTester.urlSessionResource = urlSessionResource;
+    }
+
+
+    static {
+        RestTester.getCurrentDirs();
+        try {
+            nameXmlFile = RestTester.resources + "/data.xml";
+            nameXmlFile = nameXmlFile.replaceAll("\\\\", "/");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private static Entity<?> entity = null;
+
+    public static void setArguments(String applicationName, String urlForResource, String urlForLogin, String nameConfig, String nameRequest, Entity<?> entity,
+                               String pathInResource,
+                               String parameterToResource
+
+    ) {
+        setUrlForResource(urlForResource);
+        setUrlForLogin(urlForLogin);
+        setNameRequest(nameRequest);
+        setEntity(entity);
+        setPathInResource(pathInResource);
+        setParameterToResource(parameterToResource);
+        RestTester.setApplicationName(applicationName);;
+        RestTester.setNameConfig(nameConfig);
+    }
+    public static void setArguments(String urlForResource, String urlForLogin, String nameRequest, Entity<?> entity,
+                                    String pathInResource,
+                                    String parameterToResource
+
+    ) {
+        setUrlForResource(urlForResource);
+        setUrlForLogin(urlForLogin);
+        setNameRequest(nameRequest);
+        setEntity(entity);
+        setPathInResource(pathInResource);
+        setParameterToResource(parameterToResource);
+    }
+
+    private static String urlPaymentResource="http://localhost:8080/rest/payment";
+    private static String urlSessionResource="http://localhost:8080/rest/session";
+
+    public static void setResources(String applicationName,String nameConfig
+
+    ) {
+        RestTester.setApplicationName(applicationName);;
+        RestTester.setNameConfig(nameConfig);
+    }
+
+
+    public static Context getContext() {
+        return RestTester.getContext();
+    }
+
+    @BeforeClass
+    public static void prepareData() {
+        setResources("ecom-rest","ecom-dev.properties");
+        RestTester.prepareData();
+/*
+        try {
+
+            transactionalCaller = (Caller)
+                    ctx.lookup("java:global/ecom-rest/test/TransactionBean");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+*/
+
+    }
+
+    /*
+        test SessionResource
+    */
+
+
+    @Test
+    @Order(order = 0)
+    public void setSessionUserNamePassword() {
+        if (!RestTester.GLASSFISH_ON) return;
+        try {
+            setArguments(urlPaymentResource, urlSessionResource,"get",null,"id","21213832");
+            RestTester.setSessionUserNamePassword(urlForLogin, Credentials.getUserName(Credentials.MERCHANT), Credentials.MERCHANT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Test
+    @Order(order = 1)
+    public void checkResource() {
+        if (!RestTester.GLASSFISH_ON) return;
+        try {
+            setArguments(urlPaymentResource, urlSessionResource,"get",null,"id","21213832");
+            RestTester.checkResource(urlForResource,"get","id","21213832",null);
+        } catch (Exception e) {
+            System.out.println("\n\tGot exception: " + e.getMessage());
+            System.out.println(e.getCause());
+            if (e.getMessage() != null && e.getMessage().contains("Connection refused")) {
+                System.out.println("Glassfish server not started");
+                RestTester.GLASSFISH_ON = false;
+            }
+
+        }
+
+
+        return ;
+    }
+
+    //@Test
+    //@Order(order = 3)
+/*
+    public void checkPaymentStateResource() {
+        if (!GLASSFISH_ON) return;
+        try {
+            System.out.println("nameXmlFile = " + nameXmlFile);
+            WebTarget target = client.target(getBaseURI("http://localhost:8080/payment"))
+                    .path("/status").path("/{nameFile}")
+                    .resolveTemplate("nameFile", nameXmlFile);
+            Object paymentDTO = new PaymentDTO();
+            System.out.println("Received cookie: " + cookieMap);
+            Response clientResponse = target
+                    .request(MediaType.APPLICATION_JSON)
+                    .cookie("JSESSIONID", cookieMap.get("JSESSIONID").getValue())
+                    .post(Entity.entity(paymentDTO, MediaType.APPLICATION_JSON), Response.class);
+            String jsonResponse = clientResponse.readEntity(String.class);
+            System.out.println("Received response from persister: " + jsonResponse);
+        } catch (Exception e) {
+            System.out.println("\n\tGot exception: " + e.getMessage());
+            System.out.println(e.getCause());
+            if (e.getMessage() != null && e.getMessage().contains("Connection refused")) {
+                System.out.println("Glassfish server not started");
+                GLASSFISH_ON = false;
+            }
+
+        }
+
+
+    }
+*/
+
+    //@Test
+    //@Order(order = 4)
+/*
+    public void checkPaymentDepositResource() {
+        if (!GLASSFISH_ON) return;
+        try {
+            System.out.println("nameXmlFile = " + nameXmlFile);
+            WebTarget target = client.target(getBaseURI("http://localhost:8080/payment"))
+                    .path("/deposit").path("/{nameFile}")
+                    .resolveTemplate("nameFile", nameXmlFile);
+            Object paymentDTO = new PaymentDTO();
+            System.out.println("Received cookie: " + cookieMap);
+            Response clientResponse = target
+                    .request(MediaType.APPLICATION_JSON)
+                    .cookie("JSESSIONID", cookieMap.get("JSESSIONID").getValue())
+                    .post(Entity.entity(paymentDTO, MediaType.APPLICATION_JSON), Response.class);
+            String jsonResponse = clientResponse.readEntity(String.class);
+            System.out.println("Received response from persister: " + jsonResponse);
+        } catch (Exception e) {
+            System.out.println("\n\tGot exception: " + e.getMessage());
+            System.out.println(e.getCause());
+            if (e.getMessage() != null && e.getMessage().contains("Connection refused")) {
+                System.out.println("Glassfish server not started");
+                GLASSFISH_ON = false;
+            }
+
+        }
+
+
+    }
+*/
+
+    /*
+        only for admin testing method PaymentResource changeStateExistingPayment
+    */
+    //@Test
+    //@Order(order = 1)
+/*
+    public void checkPaymentResource() {
+        if (!GLASSFISH_ON) return;
+        try {
+            WebTarget target = client.target(getBaseURI())
+                    .path("/{idPayment}")
+                    .path("/state")
+                    .path("/{state}")
+                    .resolveTemplate("idPayment", "21195528")
+                    .resolveTemplate("state", "5");
+            Object paymentDTO = new PaymentDTO();
+            System.out.println("Received cookie: " + cookieMap);
+            Response clientResponse = target
+                    .request(MediaType.APPLICATION_JSON)
+                    .cookie("JSESSIONID", cookieMap.get("JSESSIONID").getValue())
+                    .put(Entity.entity(paymentDTO, MediaType.APPLICATION_JSON), Response.class);
+            String jsonResponse = clientResponse.readEntity(String.class);
+            System.out.println("Received response from persister: " + jsonResponse);
+        } catch (Exception e) {
+            System.out.println("\n\tGot exception: " + e.getMessage());
+            System.out.println(e.getCause());
+            if (e.getMessage() != null && e.getMessage().contains("Connection refused")) {
+                System.out.println("Glassfish server not started");
+                GLASSFISH_ON = false;
+            }
+
+        }
+
+
+    }
+*/
+/*
+    only for merchant-user testing method PaymentResource findApprovedPaymentsCount
+*/
+
+    //  String urlForGetApprovedPayments = "http://localhost:8080/rest/payment";
+
+    //@Test
+    //@Order(order = 2)
+/*
+    public void testCountApprovedPayments() {
+        if (!GLASSFISH_ON) return;
+        try {
+            autorization(Credentials.getUserName(Credentials.MERCHANT), Credentials.MERCHANT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        try {
+            WebTarget target = client.target(getBaseURI(urlForGetApprovedPayments))
+                    .path("approvedPaymentsCount");
+            Long count = 0L;
+            Response clientResponse = target
+                    .request(MediaType.APPLICATION_JSON)
+                    .cookie("JSESSIONID", cookieMap.get("JSESSIONID").getValue())
+                    .get();
+            String jsonResponse = clientResponse.readEntity(String.class);
+            System.out.println("Received response from persister: " + jsonResponse);
+        } catch (Exception e) {
+            System.out.println("\n\tGot exception: " + e.getMessage());
+            System.out.println(e.getCause());
+            if (e.getMessage() != null && e.getMessage().contains("Connection refused")) {
+                System.out.println("Glassfish server not started");
+                GLASSFISH_ON = false;
+            }
+
+        }
+
+    }
+*/
+    private static URI getBaseURI(String url) {
+        return UriBuilder.fromUri(url).build();
+    }
+
+    @AfterClass
+    public static void destroyContainer() {
+        if (RestTester.client != null) {
+            RestTester.client.close();
+        }
+
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+}
